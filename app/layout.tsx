97336736100b68281a0c13d5bb0fd0ee5eae1aa5
binayakedem <binayakedem@gmail.com>
2026-07-26 15:54:12 +0545

    layout design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
-import './globals.css';
 import Navbar from '../components/navbar';
 
 export const metadata: Metadata = {
-    title: 'Ecommerce Store',
+    title: 'Sumniwa Design',
     description: 'Next.js ecommerce frontend with Firebase auth',
 };
 
